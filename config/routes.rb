86c36bea044200f14f948513896e19bf9e738f11
creynolds8 <cameron8.cr@gmail.com>
2024-06-27 21:08:44 -0700

    add edit link to index and redirect index to default

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  
   root "programming_languages#index"
+
+  get "/programming_languages", to: redirect("/")
+
   resources :programming_languages
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
